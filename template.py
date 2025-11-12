5b0a2daab608c0ae7352517009102826186823ef
“Rajeshwary-ux <katraguntaraji@gmail.com>
2025-11-12 13:03:00 -0500

    folder structure updated

---
+++ template.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+import logging
+
+
+# print(Path("a/b/c.txt"))
+
+list_of_files =[
+    ".github/workflow/.getkeep",
+    "src/_init_.py",
+    "src/components/_init_.py",
+    "src/component/data_ingestion.py",
+    "src/components/ata_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/_init_.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/_init_.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception"
+    "test/unit/_init_.py",
+    "test/integration/_init_.py",
+    "init_setup.sh",
+    "requirement.txt",
+    "requirement_dev.txt",
+    "setup.py",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb"
+
+
+]
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+      
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass  # create an empty file
